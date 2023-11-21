4d44aa3df00ad80694588a4b9d0afcc42ee21dd3
Гарифуллин Амир <amirgarifullin@gmail.com>
2023-11-21 22:24:44 +0300

    Добавьте файлы проекта.

---
+++ Lab/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    internal class Program
+    {
+        static void DoExercises()
+        {
+            Console.WriteLine("Упражнения 1 - 2\n");
+
+            BankType type = BankType.Сберегательный;
+            BankFactory factory = new BankFactory();
+            factory.CreateAccount(type);
+            factory.CreateAccount(800);
+            factory.CreateAccount(850, type);
+            factory.CreateAccount();
+            factory.DeleteAccount(3);
+            factory.Print();
+
+        }
+        static void Main(string[] args)
+        {
+            DoExercises();
+
+
+            Console.ReadKey();
+        }
+    }
+}
